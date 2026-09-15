43e358ec6b2bf83a7ad37196abc09f0f905d4daf
Adrian Milvang <adrian.milvang@gmail.com>
2026-09-15 10:32:33 +0200

    Remove depricated tsgo from ensure_installed

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -3,7 +3,7 @@ return {
   {
     'mason-org/mason-lspconfig.nvim',
     opts = {
-      ensure_installed = { "lua_ls", "rust_analyzer", "oxfmt", "oxlint", "ts_ls", "tsgo" },
+      ensure_installed = { "lua_ls", "rust_analyzer", "oxfmt", "oxlint", "ts_ls" },
       automatic_enable = false
 
     },
