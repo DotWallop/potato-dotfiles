b6523ecdb334a59188ba179367dab2404e0c0554
Adrian Milvang <adrian.milvang@gmail.com>
2026-09-15 10:28:12 +0200

    Make nvm use lts run on start

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,11 @@
 source $HOME/.config/fish/conf.d/aliases.fish
 source $HOME/.config/fish/conf.d/other.fish
 zoxide init fish | source
+nvm use lts > /dev/null
 
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
 #    # smth smth
 #end
+alias i="sudo pacman -Suy"
